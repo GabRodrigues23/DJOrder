baa606acf27636a312a16689e8f5dbfc941ccbce
GabRodrigues23 <gabrigues23@gmail.com>
2025-12-09 03:33:01 -0300

    :sparkles: add module_routes to manage view routes with modular

---
+++ lib/features/view/module_routes.dart
@@ -0,0 +1,14 @@
+import 'package:djorder/features/view/home/home_page.dart';
+import 'package:djorder/features/view/manager/manager_orders_page.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class ModuleRoutes extends Module {
+  @override
+  void routes(RouteManager r) {
+    super.routes(r);
+
+    r.child('/', child: (_) => HomePage());
+    // r.child('/settings', child: SettingsPage());
+    r.child('/manager', child: (_) => ManagerOrdersPage());
+  }
+}
