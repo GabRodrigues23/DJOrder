b44f2168fda83f4382344ca5a99434754c3abd82
GabRodrigues23 <gabrigues23@gmail.com>
2025-12-09 03:30:36 -0300

    :sparkles: add app widget builder and app_modules to manage routes with modular

---
+++ lib/core/routes/app_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'DJOrder',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Montserrat', useMaterial3: true),
+      routerConfig: Modular.routerConfig,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:djorder/core/routes/app_widget.dart';
+import 'package:djorder/core/routes/app_module.dart';
 
+void main() {
+  runApp(ModularApp(module: AppModule(), child: AppWidget()));
+}
