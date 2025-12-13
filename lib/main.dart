5e06250ed435acea0033908650b863b00019e602
GabRodrigues23 <gabrigues23@gmail.com>
2025-12-12 22:39:28 -0300

    :sparkles: add adjustment to app window initialization on desktop

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:djorder/core/routes/app_widget.dart';
 import 'package:djorder/core/routes/app_module.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
+  WindowOptions windowOptions = const WindowOptions(
+    minimumSize: Size(1280, 720),
+    center: true,
+    title: 'DJORDER',
+  );
+
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
   runApp(ModularApp(module: AppModule(), child: AppWidget()));
 }
