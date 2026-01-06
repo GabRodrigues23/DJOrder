07ff723a4e08ce1371742f62a255a88544ca051a
GabRodrigues23 <gabrigues23@gmail.com>
2026-01-06 08:10:20 -0300

    :sparkles: add missing methods to FakeOrderRepository for order management functionality

--- test/core/viewmodel/order_view_model_errors_test.dart
+++ test/core/viewmodel/order_view_model_errors_test.dart
@@ -1,6 +1,8 @@
 import 'package:djorder/features/order/interfaces/order_repository_interface.dart';
 import 'package:djorder/features/order/model/order.dart';
 import 'package:djorder/features/order/viewmodel/order_view_model.dart';
+import 'package:djorder/features/product/model/additional.dart';
+import 'package:djorder/features/product/model/product.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -14,6 +16,50 @@ class FakeOrderRepository implements OrderRepositoryInterface {
       throw Exception('Falha na conexão com o servidor');
     }
     return mockList;
+  }
+
+  @override
+  Future<void> cancelOrder(int idOrder, bool newCanceledStatus) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> changeClient(int idOrder, String newName) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> changeTable(int idOrder, int newTable) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Product>> getCatalog() {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<int> getPeopleCount(int idOrder) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> includeProduct(
+    int idPreSale,
+    int visualId,
+    Product product,
+    double quantity, [
+    List<AdditionalItem>? additionals,
+  ]) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> updatePeopleCount({
+    required int idPreSales,
+    required int peopleCount,
+  }) {
+    throw UnimplementedError();
   }
 }
 
